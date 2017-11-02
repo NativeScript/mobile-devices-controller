14357952758e640c8b24e4711db6086edd322513
Svetoslav <tsenov.svetoslav@gmail.com>
2017-11-02 17:51:18 +0200

    Tsenov/node8 (#3)
    * fix: executeCommand
    
    * fix: import
    
    * chore: bump version 1.0.0

--- lib/ios-manager.ts
+++ lib/ios-manager.ts
@@ -1,4 +1,4 @@
-import { spawn, spawnSync } from "child_process";
+import { spawn } from "child_process";
 import { resolve } from "path";
 import { existsSync, readFileSync } from "fs";
 import { waitForOutput, executeCommand, tailFilelUntil } from "./utils";
