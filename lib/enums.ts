e81e6a9ddbe91af1dab17afba72cb1aea2e3e267
SvetoslavTsenov <tsenov.svetoslav@gmail.com>
2017-11-11 00:36:09 +0200

    fix: enums

---
+++ lib/enums.ts
@@ -0,0 +1,16 @@
+export enum Platform {
+    ANDROID = "android",
+    IOS = "ios",
+}
+export enum DeviceType {
+    SIMULATOR = "simulator",
+    EMULATOR = "emulator",
+    DEVICE = "device",
+}
+export enum Status {
+    SHUTDOWN = "shutdown",
+    BOOTED = "booted",
+    BUSY = "busy",
+    UNAUTORIZED = "unautorized",
+    INVALID = "invalid",
+}
